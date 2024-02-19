206b20693cee275f659521e79536ab8e17c22c59
Falli Matteo <matteofalli05@gmail.com>
2024-02-19 17:01:54 +0100

    quindicesimo commit

--- php/getFilm.php
+++ php/getFilm.php
@@ -4,7 +4,7 @@
         $result = $connection->query($sql);
         if($result){
             if($result->num_rows > 0){
-                $string = "Film presenti nel database: <br><br><table>";
+                $string = "Film disponibili: <br><br><table>";
                 $string .= "<tr><th class='id_film'>ID</th><th>Titolo</th><th class='anno_produzione_film'>Anno produzione</th><th class='genere_film'>Genere</th></tr>";
                 while($row = $result->fetch_assoc()){
                     $string .= ("<tr><td class='id_film'>".$row["CodFilm"]."</td><td>".$row["Titolo"]."</td><td class='anno_produzione_film'>".$row["AnnoProduzione"]."</td><td class='genere_film'>".$row["Genere"]."</td></tr>");
