a2c011302b2ed05e9959548adefa07862734d6dd
Paolo Bonolis <falli.matteo@itismeucci.com>
2024-04-22 13:25:35 +0200

    ventitreesimo commit

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
                 }
             ?>
             <br>
-            <button class="access_button" onclick="window.location.href = 'pages/login.php'">Recupera il tuo account</button>
+            <button class="access_button" onclick="window.location.href = 'pages/login.php'">Usa un account esistente</button>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
